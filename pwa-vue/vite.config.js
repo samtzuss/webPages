07e6cb6c5c3304ff125bd2877cc10d26a5064c33
Sam Tzu <samtzuss@yahoo.com.tw>
2022-05-24 01:08:39 +0800

    test pwa

--- pwa-vue/vite.config.js
+++ pwa-vue/vite.config.js
@@ -7,31 +7,31 @@ export default defineConfig({
     vue(),
     VitePWA({
       mode: "development",
-      base: "/webPages/pwa-vue/",
+      base: "/pwa-vue/",
       srcDir: "src",
       filename: "sw.ts",
-      includeAssets: ["/webPages/pwa-vue/favicon.png"],
+      includeAssets: ["/pwa-vue/favicon.png"],
       strategies: "injectManifest",
       manifest: {
         name: "Test Project",
         short_name: "Test",
         theme_color: "#ffffff",
-        start_url: "/webPages/pwa-vue/",
+        start_url: "/pwa-vue/",
         display: "standalone",
         background_color: "#ffffff",
         icons: [
           {
-            src: "/webPages/pwa-vue/assets/icon-192.png",
+            src: "/pwa-vue/assets/icon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/webPages/pwa-vue/assets/icon-512.png",
+            src: "/pwa-vue/assets/icon-512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/webPages/pwa-vue/assets/icon-512.png",
+            src: "/pwa-vue/assets/icon-512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
