54f596d9b7b57bee95efee352691a4e02d198ec8
Sam Tzu <samtzuss@yahoo.com.tw>
2022-05-24 17:49:19 +0800

    pwa test

--- pwa-vue/vite.config.js
+++ pwa-vue/vite.config.js
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue"
 import { VitePWA } from "vite-plugin-pwa"
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../docs/pwa-vue'
+  },
   plugins: [
     vue(),
     VitePWA({
