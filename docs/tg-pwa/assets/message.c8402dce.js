d0bb2e285b3dc66a16f4ae3804bf0c67a1536bae
Sam Tzu <samtzuss@yahoo.com.tw>
2022-06-30 13:42:02 +0800

    i18n to docs

--- docs/tg-pwa/assets/message.c8402dce.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as a}from"./app.bb2ab747.js";const o={};function r(t,c,n,_,p,f){return s(),a("div",null,"message")}var m=e(o,[["render",r],["__file","message.vue"]]);export{m as default};
